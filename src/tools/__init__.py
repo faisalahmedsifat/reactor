2835df4b314f3eee09f3a95b59991baeb19da329
Faisal Ahmed Sifat <faisalahmed531@gmail.com>
2025-12-07 16:11:45 +0600

    feat: Add new grep and log processing tools, accompanied by dedicated test scripts.

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -4,6 +4,6 @@ src/tools/__init__.py
 Export all tools from the tools module.
 """
 
-from . import shell_tools, file_tools, web_tools, todo_tools
+from . import shell_tools, file_tools, web_tools, todo_tools, grep_and_log_tools
 
-__all__ = ["shell_tools", "file_tools", "web_tools", "todo_tools"]
+__all__ = ["shell_tools", "file_tools", "web_tools", "todo_tools", "grep_and_log_tools"]
